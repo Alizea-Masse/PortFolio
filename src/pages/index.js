8584cdcb6b4df1bc2cafc3b2d07cfcf89f3741eb
Alizéa Massé <alizea777@hotmail.fr>
2023-03-24 15:36:51 +0100

    add shop image

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,7 +70,7 @@ projets d'avenir !</p>
           <div className="p-6">
             <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">PROJET</h2>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3 dark:text-gray-300">Transmission des savoirs</h1>
-            <p className="leading-relaxed mb-3">Réalisation de la partie back-end d'un site de petites annonces. <br/> <br/>
+            <div className="leading-relaxed mb-3">Réalisation de la partie back-end d'un site de petites annonces. <br/> <br/>
 
             {/* icons */}
             <div className='flex gap-3'>
@@ -86,7 +86,7 @@ projets d'avenir !</p>
             - Back-end d'un formulaire de contact avec nodemailer, xoauth et GoogleAPI <br/> <br/>
             - Écriture de tests grâce à Jest <br/> <br/>
             - Aide sur le front-end avec React.js
-            </p>
+            </div>
             <div className="flex items-center flex-wrap ">
               <a href='https://savoirs.surge.sh/' target='_blank' className="text-blue-500 inline-flex items-center md:mb-2 lg:mb-0">Visiter le site
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -106,7 +106,7 @@ projets d'avenir !</p>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3 dark:text-gray-300">Instagram 2.0</h1>
             <p className="leading-relaxed mb-3">Réalisation en cours.</p>
             <div className="flex items-center flex-wrap">
-              <a href='' className="text-blue-500 inline-flex items-center md:mb-2 lg:mb-0">Visiter le site
+              <a href='https://instagram-copy-alpha.vercel.app/' target='_blank' className="text-red-500 inline-flex items-center md:mb-2 lg:mb-0">Visiter le site
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
